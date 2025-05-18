d493ab809ddb962f225fbc8af65e4098c1c8d1fc
ErTeamDeDama <erteamdedama@gmail.com>
2025-05-18 20:01:14 +0200

    Update route.ts

--- app/api/login-token/route.ts
+++ app/api/login-token/route.ts
@@ -3,9 +3,9 @@ import postgres from 'postgres';
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 export async function POST(req: Request) {
-  const { username, password } = await req.json();
+  const { token } = await req.json();
 
-  if (!username || !password) {
+  if (!token) {
     return Response.json(
       { message: 'Tutti i campi sono obbligatori' },
       { status: 400 }
@@ -13,21 +13,17 @@ export async function POST(req: Request) {
   }
 
   try {
-    const data = await sql`SELECT * FROM users WHERE username = ${username}`;
+    const data = await sql`SELECT * FROM token WHERE token = ${token}`;
 
     if (!data || data.length === 0) {
-      return Response.json({ message: 'Access Denied' }, { status: 401 });
+      return Response.json({ message: 'Token non valido' }, { status: 401 });
     }
 
-    if (data[0].password !== password) {
-      return Response.json({ message: 'Access Denied' }, { status: 401 });
-    }
-
-    // Login OK - imposta il cookie
+    // Token valido, imposta il cookie
     const headers = new Headers();
     headers.append(
       'Set-Cookie',
-      `risposte=${username}; HttpOnly; Max-Age=86400; Path=/;`
+      `risposte=${token}; HttpOnly; Max-Age=86400; Path=/;`
     );
 
     return new Response(
