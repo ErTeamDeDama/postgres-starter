f9aba74edf3dd7c0759aeb44c3e7bb873f65d1cf
ErTeamDeDama <erteamdedama@gmail.com>
2025-05-18 19:32:10 +0200

    Update ImageCard.tsx

--- components/ImageCard.tsx
+++ components/ImageCard.tsx
@@ -1,15 +1,7 @@
-import Image, { ImageProps } from 'next/image';
-
-interface ImageCardProps extends Omit<ImageProps, 'src' | 'alt' | 'width' | 'height'> {
-  src: string;
-  alt: string;
-  width: number;
-  height: number;
-  className?: string;
-}
+interface ImageCardProps extends React.ImgHTMLAttributes<HTMLImageElement> {}
 
 const ImageCard: React.FC<ImageCardProps> = ({ className = '', ...props }) => (
-  <Image {...props} className={`my-8 ${className}`} />
+  <img {...props} className={`my-8 ${className}`} />
 );
 
 export default ImageCard;
