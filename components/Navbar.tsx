315400b8cf1a2ff689711031081a7f4a04272258
ErTeamDeDama <erteamdedama@gmail.com>
2025-05-18 18:18:04 +0200

    s

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar: React.FC = () => {
   return (
     <nav className="flex justify-between items-center px-8 py-6 border-b border-white/10 sticky top-0 z-50 backdrop-blur-md">
       <Link href="/">
-        <Image src="/ValidaFakeWhite.png" alt="ValidaFake logo" width={30} height={40} />
+        <Image src="/ValidaFakeWhite.png" alt="ValidaFake logo" width={50} height={40} />
       </Link>
 
       <div className="space-x-6 hidden md:flex">
