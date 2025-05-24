e1f778778e3019b1fa6805c6a403500296c046f0
ErTeamDeDama <erteamdedama@gmail.com>
2025-05-24 16:32:13 +0200

    Update route.ts

--- app/api/logout/route.ts
+++ app/api/logout/route.ts
@@ -1,10 +1,14 @@
-import { headers } from 'next/headers';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  return NextResponse.json({ message: 'Logout effettuato', headers:{
-    "Set-Cookie": "session=; HttpOnly; Max-Age=0; Path=/;",
-    "Content-Type": "application/json",
-  }});
-  
+  return NextResponse.json(
+    { message: 'Logout effettuato' },
+    {
+      status: 200,
+      headers: {
+        "Set-Cookie": "session=; HttpOnly; Max-Age=0; Path=/;",
+        "Content-Type": "application/json",
+      },
+    }
+  );
 }
