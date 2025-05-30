74190f170afdd9be8f2781edd4d3dec3d1743428
ErTeamDeDama <erteamdedama@gmail.com>
2025-05-30 11:55:12 +0200

    -

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -37,22 +37,15 @@ const Navbar: React.FC = () => {
 
       {/* Menu Mobile */}
       {menuOpen && (
-  <div className="absolute top-full left-0 w-full bg-black flex flex-col items-center py-4 space-y-4 md:hidden z-50">
-    <Link href="/fake-toolkit">
-      <span onClick={() => setMenuOpen(false)} className="cursor-pointer">Rileva le Fake News</span>
-    </Link>
-    <Link href="/origins-goals">
-      <span onClick={() => setMenuOpen(false)} className="cursor-pointer">Origine e scopo</span>
-    </Link>
-    <Link href="/about">
-      <span onClick={() => setMenuOpen(false)} className="cursor-pointer">Chi siamo</span>
-    </Link>
-    <Link href="/login">
-      <span onClick={() => setMenuOpen(false)} className="font-bold cursor-pointer">Login</span>
-    </Link>
-  </div>
-)}
-
+        <div className="absolute top-full left-0 w-full bg-black flex flex-col items-center py-4 space-y-4 md:hidden z-50">
+          <Link href="/fake-toolkit" onClick={() => setMenuOpen(false)}>Rileva le Fake News</Link>
+          <Link href="/origins-goals" onClick={() => setMenuOpen(false)}>Origine e scopo</Link>
+          <Link href="/about" onClick={() => setMenuOpen(false)}>Chi siamo</Link>
+          <Link href="/login" onClick={() => setMenuOpen(false)} className="font-bold">
+            Login
+          </Link>
+        </div>
+      )}
     </nav>
   );
 };
