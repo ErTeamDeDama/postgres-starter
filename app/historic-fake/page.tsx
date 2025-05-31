29c397ded9f955ecc8ded343bf9b8abd3e71ff0c
ErTeamDeDama <erteamdedama@gmail.com>
2025-05-31 14:08:04 +0200

    Update page.tsx

--- app/historic-fake/page.tsx
+++ app/historic-fake/page.tsx
@@ -2,10 +2,13 @@ import Footer from '@/components/Footer'
 import Navbar from '@/components/Navbar'
 import Section from './components/Section'
 export default function Page(){
-    <>
+    return(
+        <>
     <Navbar />
     <Section />
     <Footer />
     </>
+    )
+    
     
 }
